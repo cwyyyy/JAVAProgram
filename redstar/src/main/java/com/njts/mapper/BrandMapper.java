7b9151a6d85ba3e4a25153f61fad427b67fb6725
CHL <99399803+cwyyyy@users.noreply.github.com>
2024-03-17 14:48:04 +0800

    Delete redstar directory

--- redstar/src/main/java/com/njts/mapper/BrandMapper.java
+++
@@ -1,7 +0,0 @@
-package com.njts.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.njts.pojo.Brand;
-
-public interface BrandMapper extends BaseMapper<Brand> {
-}
--- redstar/src/main/java/com/njts/mapper/ProductVOMapper.java
+++
@@ -1,7 +0,0 @@
-package com.njts.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.njts.vo.ProductVO;
-
-public interface ProductVOMapper extends BaseMapper<ProductVO> {
-}
